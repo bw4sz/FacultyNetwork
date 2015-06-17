f17cad2d25a40d4e8abcca37aae49cd173367a6a
bw4sz <benweinstein2010@gmail.com>
2015-06-17 14:23:17 -0400

    there seems to be a parsing error that flips author and order, if there is just one author, i made a temporary solution that combines the column and gets rid of order, but in the long run i need to understand what is causing the parser to do this, its not a efficient structure right now

---
+++ todo.R
@@ -0,0 +1,23 @@
+#TO do,
+
+Investigate here:L
+
+10340018400 is the auth ID, Year==1998, DOI is 0000000157
+
+link here: http://www-scopus-com.proxy.library.stonybrook.edu/record/display.url?eid=2-s2.0-0000000157&origin=resultslist&sort=plf-f&src=s&nlo=&nlr=&nls=&sid=186A8D736ACC425FF1E25F48ECDDB311.53bsOu7mi7A1NSY7fPJf1g%3a1150&sot=a&sdt=cl&cluster=scopubyr%2c%221998%22%2ct&sl=35&s=AU-ID%28%22Hirota%2c+Kouske%22+10340018400%29&relpos=0&relpos=0&citeCnt=24&searchTerm=AU-ID%28%5C%26quot%3BHirota%2C+Kouske%5C%26quot%3B+10340018400%29+AND+%28+LIMIT-TO%28PUBYEAR%2C1998%29+%29+#
+  
+title: Photoelectrochemical reduction of CO2 in a high-pressure CO2 + methanol medium at p-type semiconductor electrodes 
+
+something going on here.
+
+#I think its if the authors don't exist in scopus!
+
+
+DOI      Order Author Citations Year
+1 SCOPUS_ID:0000000146 6603067548      1      25.0 1995
+
+This thing has two authors. Where is the other author.
+
+http://www-scopus-com.proxy.library.stonybrook.edu/results/results.url?sort=plf-f&src=s&nlo=&nlr=&nls=&sid=186A8D736ACC425FF1E25F48ECDDB311.53bsOu7mi7A1NSY7fPJf1g%3a1740&sot=a&sdt=cl&cluster=scopubyr%2c%221995%22%2ct&sl=17&s=AU-ID%286603067548%29&origin=resultslist&zone=leftSideBar&editSaveSearch=&txGid=186A8D736ACC425FF1E25F48ECDDB311.53bsOu7mi7A1NSY7fPJf1g%3a174
+
+dbGetQuery(d$con,"SELECT * From Meta Where 'Author' == '7403348672'")
