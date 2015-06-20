ba805560f0e0db4bb212a38c2cff9ee3a82c4dae
bw4sz <benweinstein2010@gmail.com>
2015-06-20 19:19:34 -0400

    extracted data from the database and integrating visualization

--- JournalClass.R
+++ JournalClass.R
@@ -101,6 +101,9 @@ journaldf<-journaldf[!duplicated(journaldf),]
 #missing
 missj<-s[!s %in% journaldf$query]
 
+#remove duplicates
+journaldf<-journaldf[!duplicated(journaldf$title),]
+
 #write to file
 copy_to(my_db,journaldf,"journal_scopus",temporary = F)
 write.csv(journaldf,"C:/Users/Ben/Dropbox/FacultyNetwork/JournalID.csv")
